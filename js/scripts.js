fa78e8945e6d4eb69f9408918bdce89eefc32c83
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-05 16:19:58 +0100

    cleaning up JS file based on ESlint

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-
-
 var pokemonRepository = (function() {
 
   var repository = [];
   //var apiUrl = 'https://dog.ceo/api/breeds/list/all';
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  var $modalContainer = $('#modal-container')
 
   function add(pokemon) {
     repository.push(pokemon);
@@ -17,7 +14,7 @@ var pokemonRepository = (function() {
 
   //add item to list (active table)
   function addListItem(item) {
-    var listContainer = $('.pokemon-list');
+    var listContainer = $('.pokemon-list'); // eslint-disable no-console
     //var listItem = $('<li class="list-group-item"></li>');
     //listContainer.append(listItem);
     var listButton = $('<button type="button" class="btn btn-light col-4 col-sm-3 col-md-2 poke-list" data-toggle="modal" data-target="#pokemon-modal"></button>');
@@ -38,9 +35,10 @@ var pokemonRepository = (function() {
         add(pokemon);
       });
     }).catch(function (e) {
+      /* eslint-disable no-console */
       console.log(e);
     })
-  };
+  }
   //load item details from API (url obtained from previous api)
   function loadDetails(item) {
     var url = item.detailsUrl;
@@ -52,7 +50,7 @@ var pokemonRepository = (function() {
     }).catch(function(e) {
       console.error(e);
     });
-  };
+  }
   //show modal and fill it with info from loadDetail function
   function showDetails (item) {
     loadDetails(item).then( () => {
@@ -77,10 +75,10 @@ var pokemonRepository = (function() {
 
     var abilitiesArray = [];
     function addAbilities(item) {
-      for (i = 0; i < item.abilities.length; i++) {
-        abilitiesArray.push(' ' + item.abilities[i].ability.name);
+      for (i = 0; i < item.abilities.length; i++) { // eslint-disable-line no-undef
+        abilitiesArray.push(' ' + item.abilities[i].ability.name); // eslint-disable-line no-undef
       }
-    };
+    }
     addAbilities(item);
 
     var modalInfo = $('<p></p>');
@@ -92,13 +90,13 @@ var pokemonRepository = (function() {
     modalImage.attr('src', item.imageUrl);
     $imageContainer.append(modalImage);
 
-  })};
+  })}
 
   //function to make the search field work
   $(document).ready(function(){
-    $("#search-field").on("keyup", function() {
+    $('#search-field').on('keyup', function() {
       var value = $(this).val().toLowerCase();
-      $(".poke-list").filter(function() {
+      $('.poke-list').filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
